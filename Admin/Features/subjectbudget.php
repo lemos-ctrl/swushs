4f16afc12b14d966b85e484d7df74daa668b4907
Kurt Imperial <76236273+kurtimperial23@users.noreply.github.com>
2024-03-31 09:20:04 +0800

    working link tracking just needs to touch it up a little

---
+++ Admin/Features/subjectbudget.php
@@ -0,0 +1,112 @@
+<?php require_once('../../includes/cdn.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHS Faculty</title>
+
+    <link rel="stylesheet" href="../../Styles/styles.css">
+    <script src="../../Scripts/script.js"></script>
+</head>
+
+<body>
+
+<div class="wrapper">
+<?php include('../includes/sidebar.php'); ?>
+
+    
+<div class="main">
+        <nav class="navbar custom-toggler navbar-expand px-3 border-bottom">
+            <button class="btn" id="sidebar-toggle" type="button">
+                <span class="navbar-toggler-icon "></span>
+            </button>
+            <div class="navbar-collapse navbar p-0 d-flex justify-content-end align-items-center">
+                <span>Welcome back <b>Kurt</b>!</span>
+                <a href="#" class="las la-user-circle ps-2"></a>
+            </div>
+        </nav>
+
+        <main class="content px-3 py-2">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card flex-fill border-0 illustration">
+                            <div class="card-body p-0 d-flex flex-fill">
+                                <div class="row g-0 w-100">
+                                    <div class="col-6">
+                                        <div class="p-3 m-1">
+                                            <h4>School Year</h4>
+                                            <p class="mb-0">Admin Dashboard </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 d-flex">
+                        <div class="card flex-fill border-0">
+                            <div class="card-body py-4">
+                                <div class="d-flex align-items-start">
+                                    <div class="flex-grow-1">
+                                        <h4 class="mb-2">
+                                            
+                                        </h4>
+                                        <p class="mb-2">
+                                            
+                                        </p>
+                                        <div class="mb-0">
+                                            <span class="badge text-success me-2">
+                                                <p>olah</p>
+                                            </span>
+                                            <span class="text-muted">
+                                                <p>helo</p>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <!-- <a href="#" class="theme-toggle">
+            <i class="fa-regular fa-moon"></i>
+            <i class="fa-regular fa-sun"></i>
+        </a> -->
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row text-muted">
+                    <div class="col-6 text-start">
+                        <p class="mb-0">
+                            <a href="#" class="text-muted">
+                                
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-6 text-end">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a href="#" class="text-muted">Contact</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="text-muted">About Us</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="text-muted">Terms</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="text-muted">Booking</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+
+</body>
+</html>
--- Admin/includes/sidebar.php
+++ Admin/includes/sidebar.php
@@ -3,92 +3,101 @@ $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <aside id="sidebar" class="js-sidebar">
-            <!-- Content For Sidebar -->
-            <div class="h-100 sidebar-background">
-                <div class="sidebar-logo">
-                    <a href="#">Logo</a>
-                </div>
-                <ul class="sidebar-nav">
-                    <li class="sidebar-header">
-                        Admin Elements
-                    </li>
-                    <li class="sidebar-item d-flex align-items-center">
-                        <a href="./dashboard.php" class="sidebar-link">
-                            <i class="fa-solid fa-list pe-2"></i>
-                            Dashboard
-                        </a>
+    <!-- Content For Sidebar -->
+    <div class="h-100 sidebar-background">
+        <div class="sidebar-logo">
+            <a href="#">Logo</a>
+        </div>
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Admin Elements
+            </li>
+            <li class="sidebar-item d-flex align-items-center <?php if ($current_page == 'dashboard.php') echo 'active'; ?>">
+                <a href="./dashboard.php" class="sidebar-link">
+                    <i class="fa-solid fa-list pe-2"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed <?php if ($current_page == 'schoolyear.php' || $current_page == 'subjectbudget.php') echo 'active'; ?>" data-bs-target="#pages" data-bs-toggle="collapse"
+                    aria-expanded="<?php if ($current_page == 'schoolyear.php' || $current_page == 'subjectbudget.php') echo 'true'; else echo 'false'; ?>"><i class="fa-solid fa-file-lines pe-2"></i>
+                    Curriculum
+                </a>
+                <ul id="pages" class="sidebar-dropdown list-unstyled <?php if ($current_page == 'schoolyear.php' || $current_page == 'subjectbudget.php') echo 'show'; ?>" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="../Features/schoolyear.php" class="sidebar-link sidebar-link-child <?php if ($current_page == 'schoolyear.php') echo 'active'; ?>">School Years</a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#pages" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-solid fa-file-lines pe-2"></i>
-                            Curriculum
-                        </a>
-                        <ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="../schoolyear.php" class="sidebar-link sidebar-link-child">School Years</a>
-                            </li>
-                            <li class="sidebar-item">
-                                <a href="../subjectbudget.php" class="sidebar-link sidebar-link-child">Subject Budget</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-regular fa-user pe-2"></i>
-                            People
-                        </a>
-                        <ul id="posts" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item ">
-                                <a href="../Features/faculty.php" class="sidebar-link sidebar-link-child">Faculty</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
-                            Timetable Generation
-                        </a>
-                        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link sidebar-link-child">Load Scheduling</a>
-                            </li>
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link sidebar-link-child">Faculty Loading</a>
-                            </li>
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link sidebar-link-child">Room Assignment</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-header">
-                        Resources
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed" data-bs-target="#multi" data-bs-toggle="collapse"
-                            aria-expanded="false"><i class="fa-solid fa-share-nodes pe-2"></i>
-                            Multi Dropdown
-                        </a>
-                        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link collapsed" data-bs-target="#level-1"
-                                    data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
-                                <ul id="level-1" class="sidebar-dropdown list-unstyled collapse">
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Level 1.1</a>
-                                    </li>
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Level 1.2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="../index.php" class="sidebar-link">
-                            <i class="fa-solid fa-list pe-2"></i>
-                            Logout
-                        </a>
+                        <a href="../Features/subjectbudget.php" class="sidebar-link sidebar-link-child <?php if ($current_page == 'subjectbudget.php') echo 'active'; ?>">Subject Budget</a>
                     </li>
                 </ul>
-            </div>
-        </aside>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse"
+                    aria-expanded="<?php if ($current_page == 'faculty.php') echo 'true'; else echo 'false'; ?>"><i class="fa-regular fa-user pe-2"></i>
+                    People
+                </a>
+                <ul id="posts" class="sidebar-dropdown list-unstyled <?php if ($current_page == 'faculty.php') echo 'show'; ?>" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="../Features/faculty.php" class="sidebar-link sidebar-link-child <?php if ($current_page == 'faculty.php') echo 'active'; ?>">Faculty</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
+                    aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
+                    Timetable Generation
+                </a>
+                <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-link-child">Load Scheduling</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-link-child">Faculty Loading</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-link-child">Room Assignment</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="sidebar-header">
+                Resources
+            </li>
+            
+            <li class="sidebar-item">
+                <a href="../../index.php" class="sidebar-link">
+                    <i class="fa-solid fa-list pe-2"></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if any dropdown contains the active class
+    const activeDropdown = document.querySelector('.sidebar-dropdown.show');
+    if (activeDropdown) {
+        // Expand the dropdown
+        const parentCollapse = activeDropdown.closest('.sidebar-item').querySelector('[data-bs-toggle="collapse"]');
+        if (parentCollapse) {
+            parentCollapse.setAttribute('aria-expanded', 'true');
+        }
+    }
+
+    // Save the state of the dropdown in local storage or cookie
+    const toggleButtons = document.querySelectorAll('[data-bs-toggle="collapse"]');
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const expanded = this.getAttribute('aria-expanded');
+            const parent = this.getAttribute('data-bs-target');
+            if (expanded === 'true') {
+                localStorage.setItem(parent, 'expanded');
+            } else {
+                localStorage.removeItem(parent);
+            }
+        });
+    });
+});
+</script>
