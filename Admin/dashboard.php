900398cb72d8a30e5662ed9b58eeb3900dac3627
SWU000ZY\User <Admin>
2024-04-03 15:05:45 +0800

    added table but unfinished

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -5,6 +5,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>SWU-SHS AMS Admin Dashboard</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             <link rel="stylesheet" href="../Styles/dashboard.css" />
               <!-- Fontawesome CDN Link -->
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
@@ -50,7 +51,13 @@
                                 <div id="item-0">
                                   <canvas id="chartjs-bar"></canvas>
                                 </div>
-                                <div id="item-1">&nbsp;</div>
+                                <div id="item-1">
+                                  <div class="table-responsive">
+                                    <table class="table">
+                                      ...
+                                    </table>
+                                  </div>
+                                </div>
                                 <div id="item-2">&nbsp;</div>
                                 <div id="item-3">&nbsp;</div>
                             </div>
@@ -59,6 +66,7 @@
 
                           <script src="../Scripts/dashboard-main.js"></script>
                             <script src="https://unpkg.com/@adminkit/core@latest/dist/js/app.js"></script>
-                              <script src="../Scripts/chart.js"></script>
+                              <script src="../Scripts/chart.js"></script>+
+                                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                   </body>
   </html>
