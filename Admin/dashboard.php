2a39d1658284a9b00bedbc03de651ec386240db5
lemos-ctrl <gabuterolemuel@gmailcom>
2024-04-08 23:07:32 +0800

    ambot ani yawa

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -12,31 +12,28 @@ include_once '../includes/cdn.php';?>
 <div class="wrapper">
     <?php include '../Admin/includes/sidebar.php';?>
     <div class="main">
-            <nav class="navbar custom-toggler navbar-expand px-3 border-bottom">
-                <button class="btn" id="sidebar-toggle" type="button">
-                    <span class="navbar-toggler-icon "></span>
-                </button>
-                <div class="navbar-collapse navbar p-0 d-flex justify-content-end align-items-center">
-                    <span>Welcome back <b>Lemuel</b>!</span>
-                    <a href="#" class="las la-user-circle ps-2"></a>
-                </div>
-            </nav>
+        <nav class="navbar custom-toggler navbar-expand-lg px-3 border-bottom">
+            <button class="btn" id="sidebar-toggle" type="button">
+                <span class="navbar-toggler-icon "></span>
+            </button>
+            <div class="navbar-collapse navbar p-0 d-flex justify-content-end align-items-center">
+                <span>Welcome back <b>Lemuel</b>!</span>
+                <a href="#" class="las la-user-circle ps-2"></a>
+            </div>
+        </nav>
 
-            <main class="content px-3 py-2 ">
-                    <div class="container-fluid ">
-                        <div class="row">
-                            <div class="col-md-10  ">
-                                <canvas id="myChart" class="py-1 card"></canvas>
-                            </div>
-                            
-                            <div class="col-md-2">
-                                <div class="col-md-1 card" style="width: 15rem;">
-                                        <h6 class="text-center my-3">ATTENDANCE TODAY</h6>
-
-                                    <ul class="list-unstyled news-list mt-3">
-                                        
-                                    
-                                        <li class="row mx-0 mb-4">
+        <main class="content px-3 py-2 ">
+            <div class="container-fluid ">
+                <div class="row">
+                    <div class="col-md-8 col-lg-10">
+                        <canvas id="myChart" class="py-1 card"></canvas>
+                    </div>
+                    <div class="col-md-4 col-lg-2">
+                        <div class="card">
+                            <h6 class="text-center my-3">ATTENDANCE TODAY</h6>
+                            <ul class="list-unstyled news-list mt-3">
+                                <!-- Your list items -->
+                                <li class="row mx-0 mb-4">
                                             <a class="col-1">
                                                 <div class="mt-3 mx-2 fa-regular fa-user "></div>
                                             </a>
@@ -161,130 +158,91 @@ include_once '../includes/cdn.php';?>
                                                     </div>
                                                 </div>
                                         </li>
-
-                                        
-
-
-                                    </ul>
-                                </div>
-                            </div>
+                            </ul>
                         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-                        
-                        <div class="row ">
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/check.png" alt="">
-                                        <h3 class="mt-3 mb-3">Checked In</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">36</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/x.png" alt="">
-                                        <h3 class="mt-3 mb-3">Not Checked In</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">36</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/onleave.png" alt="">
-                                        <h3 class="mt-3 mb-3">On-Leave</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">0</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/weekoff.png" alt="">
-                                        <h3 class="mt-3 mb-3">Weekly Off</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">0</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/checkout.png" alt="">
-                                        <h3 class="mt-3 mb-3">Checked Out</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">0</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div>
-                                    <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
-                                        <img src="../Resources/widgetlogo/holiday.png" alt="">
-                                        <h3 class="mt-3 mb-3">Holiday</h3>
-                                        <p class="mb-0 ">
-                                            <span class="text-nowrap">0</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-                        
-                        <div class="row d-flex ms-0">
-                            <div class="col-md-6 card">
-                                Exceptions
-                            </div>
-                            <div class="col-md-6 card">
-                                Pending Requests
-                            </div>
-                        </div>
-
-
-
-
                     </div>
-            </main>          
-        </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-2">
+                        <div class="px-3 py-3 bg-white d-flex flex-column justify-content-center align-items-center mb-3 card">
+                            <img src="../Resources/widgetlogo/check.png" alt="">
+                                <h3 class="mt-3 mb-3">Checked In</h3>
+                                    <p class="mb-0 ">
+                                <span class="text-nowrap">36</span>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            Exceptions
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            Pending Requests
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
+</div>
     <script src="../Scripts/barchart.js"></script>
 </body>
 </html>
